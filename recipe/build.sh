af91dc4d4cd81a720c049d07a7abd2cc4e12ec5a
Rhys Poulton <rhyspoulton92@gmail.com>
2021-04-08 17:15:17 +0100

    Added mkdir to ensure the activate directory exists

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,7 +18,10 @@ ninja -v
 ninja install
 
 # Install activate/deactivate stripts
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/cm-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/cm-deactivate.sh
+ACTIVATE_DIR=${PREFIX}/etc/conda/activate.d
+DEACTIVATE_DIR=${PREFIX}/etc/conda/deactivate.d
+mkdir -p ${ACTIVATE_DIR}
+mkdir -p ${DEACTIVATE_DIR}
+
+cp ${RECIPE_DIR}/scripts/activate.sh ${ACTIVATE_DIR}/cm-activate.sh
+cp ${RECIPE_DIR}/scripts/deactivate.sh ${DEACTIVATE_DIR}/cm-deactivate.sh
