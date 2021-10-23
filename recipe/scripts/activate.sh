cf8dd27647f62e344692a7f665da46d9d985bc8c
Rhys Poulton <rhyspoulton92@gmail.com>
2021-10-23 16:30:37 +0100

    Update to de-/activate scripts
    The activate script will now check if the CMDOMAIN varible is already
    defined, only if it is not defined will it set the CMDOMAIN=Conda, CMMGR
    and CmDomains file. Likewise the deactivate script will check if the
    CMDOMAIN==Conda before unsetting CMDOMAIN and CMMGR

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
-# Store existing env vars so we can restore them later
-if [ ! -z "$CMDOMAIN" ]; then
-    export _CONDA_SET_CMDOMAIN=$CMDOMAIN
-fi
-export CMDOMAIN=Conda
+# Check if CMDOMAIN varible is set to check if we are already in a cm domain if not
+# then create a custom conda cm domain that can send/ recieve messages
+if [ -z "${CMDOMAIN+x}" ]; then
+    export CMDOMAIN=Conda
 
-if [ ! -z "$CMMGR" ]; then
-    export _CONDA_SET_CMMGR=$CMMGR
-fi
+    if [ ! -z "${CMMGR+x}" ]; then
+        export _CONDA_SET_CMMGR=$CMMGR
+    fi
 
-# Check if varible CMTMPDIR exist, otherwise checks if TMPDIR varible is set and if not sets it to /tmp
-if [ ! -z "$CMTMPDIR" ]; then
-    TMPDIR=$CMTMPDIR
-elif [ -z "$TMPDIR" ]; then
-    TMPDIR=/tmp
-fi
+    mkdir -p $CONDA_PREFIX/share/cm/mgr
+    export CMMGR=$CONDA_PREFIX/share/cm/mgr
 
-mkdir -p $TMPDIR/cm/mgr
-export CMMGR=$TMPDIR/cm/mgr
-
-#Create the cmdomains file if it does not already exist
-CMDOMAINS=$TMPDIR/cm/mgr/CmDomains
-if [ ! -f "$CMDOMAINS" ]; then 
-    echo "Conda localhost  19000 19001 899 $TMPDIR/cm" > $TMPDIR/cm/mgr/CmDomains
+    #Create the cmdomains file if it does not already exist
+    CMDOMAINFILE=$CONDA_PREFIX/share/cm/mgr/CmDomains
+    if [ ! -f "$CMDOMAINFILE" ]; then
+        echo "Conda localhost  19000 19001 899 $CONDA_PREFIX/share/cm" > $CONDA_PREFIX/share/cm/mgr/CmDomains
+    fi
 fi
--- recipe/scripts/deactivate.sh
+++ recipe/scripts/deactivate.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # Restore previous env vars if any
-export CMDOMAIN=
-if [ ! -z "$_CONDA_SET_CMDOMAIN" ]; then
-    export CMDOMAIN=$_CONDA_SET_CMDOMAIN
-    export _CONDA_SET_CMDOMAIN=
-fi
-
-export CMMGR=
-if [ ! -z "$_CONDA_SET_CMMGR" ]; then
-    export CMMGR=$_CONDA_SET_CMMGR
-    export _CONDA_SET_CMMGR=
+if [ "${CMDOMAIN}" = "Conda" ]; then
+    unset CMDOMAIN
+    unset CMMGR
+    if [ -z "$_CONDA_SET_CMMGR" ]; then
+        export CMMGR=$_CONDA_SET_CMMGR
+        unset _CONDA_SET_CMMGR
+    fi
 fi
